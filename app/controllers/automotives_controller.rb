e6c65e0296adf6165e10de97ef5162b1736a9242
Steven Masse <semasse2000@yahoo.com>
2019-09-05 19:49:20 -0500

    Fixing attempt 2

--- app/controllers/automotives_controller.rb
+++
@@ -1,9 +0,0 @@
-class AutomotivesController < ApplicationController
-  def index
-    @makes = ['Chrysler', 'Dodge', 'Jeep', 'Ram']
-  end
-
-  def show
-    @inventory = Inventory.find(1)
-  end
-end
