b8c3a01ae16db49002db8d0a211b116aa984c125
Joshua Swartzell <joshuas193@live.com>
2019-09-26 20:22:54 -0500

    A few styilng changes

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,9 @@
 class Stock < ApplicationRecord
+
+  validates :make, :model, :color, :year,:saleprice, presence: true
+  validates :dealerprice, :saleprice, :profit, numericality: {greater_than_or_equal_to: 0}
+  validates :url, allow_blank: true, format: {
+  with:    /\w+\.(gif|jpg|png)\z/i,
+  message: "must reference a GIF, JPG, or PNG image"
+  }
 end
