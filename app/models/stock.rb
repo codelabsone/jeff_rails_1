4eeb5e7d0fe18dbf9b6c7982c7ada8ed16371876
Steven Masse <semasse2000@yahoo.com>
2019-09-29 23:45:54 -0500

    Fixed model so that it allowed jpeg as an image source for validation so that db:seed worked.

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,7 +3,7 @@ class Stock < ApplicationRecord
   validates :make, :model, :color, :year,:saleprice, presence: true
   validates :dealerprice, :saleprice, :profit, numericality: {greater_than_or_equal_to: 0}
   validates :url, allow_blank: true, format: {
-  with:    /\w+\.(gif|jpg|png)\z/i,
+  with:    /\w+\.(gif|jpg|png|jpeg)\z/i,
   message: "must reference a GIF, JPG, or PNG image"
   }
 end
