01aedc80fe293448fb8cbc341f27af1dc3a593bf
Steven Masse <semasse2000@yahoo.com>
2019-09-30 00:01:15 -0500

    Changed Image filed size from 200x200 to 100x100.

--- app/helpers/automotives_helper.rb
+++ app/helpers/automotives_helper.rb
@@ -1,9 +1,9 @@
 module AutomotivesHelper
   def image_for(stock)
     if stock.url.blank?
-      image_tag('th.jpg', size: "200x200")
+      image_tag('th.jpg', size: "100x100")
     else
-      image_tag(stock.url, size: "200x200")
+      image_tag(stock.url, size: "100x100")
   end
 end
 end
