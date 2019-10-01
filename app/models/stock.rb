63404fb02bbf046c76743c99cae171e5ec101aeb
Joshua Swartzell <joshuas193@live.com>
2019-09-30 19:40:44 -0500

    added flashes partials

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,7 @@
 class Stock < ApplicationRecord
 
   validates :make, :model, :color, :year,:saleprice, presence: true
-  validates :dealerprice, :saleprice, :profit, numericality: {greater_than_or_equal_to: 0}
+  validates :dealerprice, :saleprice, :profit, allow_blank: true, numericality: {greater_than_or_equal_to: 0}
   validates :url, allow_blank: true, format: {
   with:    /\w+\.(gif|jpg|png|jpeg)\z/i,
   message: "must reference a GIF, JPG, or PNG image"
