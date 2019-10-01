ff7bd3859aacc21ab2d197f76abec0fa3a7e37e2
Joshua Swartzell <joshuas193@live.com>
2019-09-30 20:17:29 -0500

    added contacts route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "automotives#index"
-
+  get "/automotives/contacts", to: "automotives#contacts", as: "contacts"
   resources :automotives
   resources :stocks
 end
