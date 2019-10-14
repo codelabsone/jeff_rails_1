d45c2e03eeb64119ed3aef5d5e2e273225b9da81
Steven Masse <semasse2000@yahoo.com>
2019-10-14 18:26:35 -0500

    Schema revised.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191010235555) do
+ActiveRecord::Schema.define(version: 20191014231356) do
 
   create_table "stocks", force: :cascade do |t|
     t.string "make"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20191010235555) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.boolean "admin", default: false
   end
 
 end
