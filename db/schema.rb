4ce9e0fd6f584abeb6930a5acd08f7371108f478
Joshua Swartzell <joshuas193@live.com>
2019-10-14 20:16:06 -0500

    Added changes to navbar and fixed errors

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,20 @@
 
 ActiveRecord::Schema.define(version: 20191014231356) do
 
+  create_table "inventories", force: :cascade do |t|
+    t.string "make"
+    t.string "model"
+    t.string "color"
+    t.integer "year"
+    t.integer "dealerprice"
+    t.integer "saleprice"
+    t.integer "profit"
+    t.string "inventorystatus"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "url"
+  end
+
   create_table "stocks", force: :cascade do |t|
     t.string "make"
     t.string "model"
