39a6867f16b9365954d9c9dcb8e5fb2055d843f0
Steven Masse <semasse2000@yahoo.com>
2019-10-22 08:15:40 -0500

    Scrapping rails 5 project and rebuilding under rails 6 where activestorage is now working.

--- app/helpers/automotives_helper.rb
+++
@@ -1,9 +0,0 @@
-module AutomotivesHelper
-  def image_for(stock)
-    if stock.url.blank?
-      image_tag('th.jpg', size: "160x90")
-    else
-      image_tag(stock.url, size: "160x90")
-  end
-end
-end
